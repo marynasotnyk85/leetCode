f49e663314af0d3c56fb9dc47b5a3f8a9d80f18b
Maryna <marynasotnyk85@gmail.com>
2026-03-18 09:19:02 +0100

    387. First Unique Character in a String

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/** 387. First Unique Character in a String */
+
+var firstUniqChar = function(s) {
+    const freq = {};
+
+    for (const ch of s){
+        freq[ch] = (freq[ch] || 0)+1;
+    }
+    for (let i= 0; i < s.length; i++){
+        if (freq[s[i]] === 1) {
+            return i;
+        }  
+    }
+    return -1;
+};
+
 /** 383. Ransom Note */
 const ransomNote = "aa";
  const magazine = "ab";
