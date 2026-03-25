7dbc538de175ac05c5d99e8f9d72df71654812c3
Maryna <marynasotnyk85@gmail.com>
2026-03-25 14:45:25 +0100

    232. Implement Queue using Stacks 2

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+/** 21. Merge Two Sorted Lists */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+var mergeTwoLists = function(list1, list2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+
+  while(list1 && list2){
+    if (list1.val <= list2.val){
+        current.next = list1;
+        list1= list1.next;
+    } else {
+        current.next = list2;
+        list2 = list2.next;
+    }
+    current = current.next;
+  } 
+    if (list1 !== null) {
+        current.next = list1;
+    } else {
+        current.next = list2;
+    }
+
+    return dummy.next;
+}
+
+
+let list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+let list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+mergeTwoLists(list1, list2);
+
 /** 232. Implement Queue using Stacks */
 class MyQueue4 {
     constructor() {
