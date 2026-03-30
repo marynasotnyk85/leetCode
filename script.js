9495b955b2b0ba8dc96527bd3b7fc9d743fa7d12
Maryna <marynasotnyk85@gmail.com>
2026-03-30 14:05:30 +0200

    104. Maximum Depth of Binary Tree

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+/** 104. Maximum Depth of Binary Tree */
+var maxDepth = function(root){
+  if(!root) return 0;
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+
+
+var maxDepth2 = function(root) {
+    if (!root) {
+        return 0;
+    }
+
+    const queue = [];
+    queue.push(root);
+    let depth = 0;
+
+    while (queue.length > 0) {
+        depth++;
+
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+
+    return depth;    
+};
+
 /** 101. Symmetric Tree */
 var isSymmetric = function(root) {
     const isMirror = (n1, n2) => {
