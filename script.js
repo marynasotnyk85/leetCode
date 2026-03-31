c16c8d212a169e398f746baa63c18455fcc8a179
Maryna <marynasotnyk85@gmail.com>
2026-03-31 10:33:09 +0200

    704. Binary Search

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+/** 704. Binary Search 
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ Output: 4
+Explanation: 9 exists in nums and its index is 4
+*/
+var search = function(nums, target) {
+    let lo = 0, hi = nums.length-1;
+   
+    while (lo < hi) {
+        let mid = Math.floor((lo + hi)/2);
+
+        if (nums[mid] === target ) return mid;
+        if (nums[mid] < target) lo = mid +1;
+        else hi = mid -1;
+    }
+    return -1;
+};
+
+/** 700. Search in a Binary Search Tree
+ * BST =   node val > all nodes in left
+ *         node val <= all nodes in right
+ */
+var searchBST = function(root,val){
+    if (!root) return null;
+
+    if (root.val === val ) return root;
+
+    else if (root.val > val) {
+        return searchBST(root.left, val);
+    } else {
+        return searchBST(root.right, val);
+    }
+}
 /** 617. Merge Two Binary Trees */
 var mergeTree = function (root1, root2) {
     if ( !root1 ) return root2;
