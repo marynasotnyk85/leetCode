24ec6009ccbf6d6e1d4dac63f1db00049847641b
Maryna <marynasotnyk85@gmail.com>
2026-04-01 11:44:15 +0200

    733. Flood Fill

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/** 733. Flood Fill */
+var floodFill = function(image, sr, sc, color) {
+    const old = image[sr][sc];
+    if (old === color) return image;
+    const m = image.length, n = image[0].length;
+
+    function dfs(i, j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || image[i][j] !== old) return;
+        image[i][j] = color;
+        dfs(i + 1, j);
+        dfs(i - 1, j);
+        dfs(i, j + 1);
+        dfs(i, j - 1);
+    }
+
+    dfs(sr, sc);
+    return image;
+};
 /** 653. Two Sum IV - Input is a BST */
 var findTarget = function(root, k){
     const seen = new Set();
